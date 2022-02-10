f91c4dda8f09845c5a328bd64173084a49efd85e
Orientir <ya.orient@gmail.com>
2022-02-10 09:13:12 +0200

    password reset

--- TutorService/account/urls.py
+++ TutorService/account/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
     path('signup/', views.signup, name="signup"),
-    path(r"^password/reset/$", views.password_reset, name="reset-password"),
+    path('password/reset/', views.password_reset, name="reset-password"),
 ]
